3d3d78b9ba1aec1510cfcae097250aef2fd41f75
asher lee <asherlee.bxl@gmail.com>
2026-01-10 15:31:17 +0800

    Email object

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-import email
 from email import policy
-from email.parser import BytesParser
+from email.parser import BytesParser, HeaderParser
 from bs4 import BeautifulSoup
+from string import printable
 class Email:
-    body = ""
-    subject = ""
-    sender = ""
-    header = ""
-    raw = None
-    def __init__(self,email:str=None,attachments:dict=None):
-        extracted = self.extract(email,attachments)
-        self.body = extracted[0]
-        self.subject = extracted[1]
-        self.sender = extracted[2]
-        self.header = extracted[3]
-        self.raw = email
+    def __init__(self,email_path:str=None):
+        headers = self.__extract_headers(email_path)
+        self.text:str = self.__extract_body(email_path)[0]
+        self.raw = self.__extract_body(email_path)[1]
+        self.subject:str = headers['Subject'] 
+        self.sender:str = headers['From'] 
+        self.header = headers 
     
-    #extraction logic for eml files 
-    def Extract_eml(email_path:str=None):
-        text = ""
-        title = ""
-        sender = ""
-        header = ""
+    def __extract_body(self,email_path:str=None):
         with open(email_path,'rb') as file:
             raw = BytesParser(policy=policy.default).parse(file)
         #removes HTML tags 
         body = raw.get_body().get_content()
-        text = BeautifulSoup(body,"lxml").text
-        title = raw['subject']
-        sender = raw['from']
-        header = raw['header']
-
-        return (text,title,sender,header,raw)
+        txt:str = BeautifulSoup(body,"lxml").text
+        txt = "\n".join(item for item in txt.split('\n') if item)
+        return txt,raw
+    
+    def __extract_headers(self,email_path:str=None):
+        with open(email_path,'r') as file:
+            raw = HeaderParser().parse(file)
+        raw_dict = {}
+        for item in raw.items():
+            raw_dict[item[0]] = item[1]
+        return raw_dict
+    
+    def __repr__(self):
+        return f"Email<Subject:{self.subject},Sender:{self.sender}>"
 
 def detect_keyword(text:str="",keywords:list=[],keyphrases:list=[]):
     words = text.split(' ')
