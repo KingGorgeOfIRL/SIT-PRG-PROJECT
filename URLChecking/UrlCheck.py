83c69d4aaef4d69c8a13f2874a6e875ead4cb559
asher lee <asherlee.bxl@gmail.com>
2026-01-31 18:31:23 +0800

    autotesting done & Email centralised

--- URLChecking/UrlCheck.py
+++ URLChecking/UrlCheck.py
@@ -3,137 +3,7 @@ from requests import get, post
 from datetime import datetime, timezone
 from vtapi3 import VirusTotalAPIUrls
 from json import loads
-
-from os import listdir, remove, path
-from email import policy
-from email.parser import Parser, HeaderParser
-from html.parser import HTMLParser
-from io import StringIO
-import base64
-
-class MLStripper(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.strict = False
-        self.convert_charrefs= True
-        self.text = StringIO()
-
-    def handle_data(self, d):
-        self.text.write(d)
-
-    def get_data(self):
-        return self.text.getvalue()
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
-
-class Email:
-    def __init__(self,email_path:str):
-        self.email_path:str = email_path
-        headers = self.__extract_headers()
-        extract = self.__extract_body()
-        self.text:str = extract[0]
-        self.attachment_header = extract[1]
-        self.raw = extract[2]
-        self.urls = extract[3]
-        self.subject:str = headers['Subject'] 
-        self.sender:str = headers['From'] 
-        self.headers:dict = headers 
-        
-    #extracts all body text free of HTML tags
-    def __extract_body(self):
-        with open(self.email_path,'r') as file:
-            raw = Parser(policy=policy.default).parse(file)
-        attachment_header = []
-        urls = []
-        plain_text:str = None
-        for part in raw.walk():
-            if part.is_attachment():
-                # print("attachment found",part.get('Content-Disposition'))
-                if "base64" in part.get("Content-Transfer-Encoding"):
-                    attachment_header.append(self.__bs64_save_attachments(part.get_payload(),part.get('Content-Disposition')))
-                else:
-                    print("cannot save file. not the right encoding")
-            elif 'text/plain' in part.get('Content-Type'):
-                plain_text = str(part.get_payload(decode=True))
-            elif 'text/html' in part.get('Content-Type'):
-                plain_text = strip_tags(str(part.get_payload(decode=True).decode("utf-8")))
-                html_content = part.get_payload(decode=True).decode("utf-8", errors="ignore")
-
-                ################################## pick which method is better
-                # extract urls - method 1
-                # if "href=" in html_content.lower():
-                #     import re
-                #     urls = re.findall(r'href=["\'](.*?)["\']', html_content)
-
-                #     print(urls)
-
-                # extract urls - method 2
-                if "href=" in html_content.lower():
-                    urls = []
-
-                    start = 0
-                    while True:
-                        href_pos = html_content.find('href="', start)
-                        if href_pos == -1:
-                            break
-
-                        href_pos += len('href="')
-                        end_pos = html_content.find('"', href_pos)
-
-                        urls.append(html_content[href_pos:end_pos])
-                        start = end_pos
-
-                    #print(urls)
-                        
-        return plain_text,attachment_header,raw,urls
-    
-    #extract all email headers as a dictionary
-    def __extract_headers(self):
-        with open(self.email_path,'r') as file:
-            raw = HeaderParser().parse(file)
-        raw_dict = {}
-        for item in raw.items():
-            raw_dict[item[0]] = item[1]
-        return raw_dict
-    
-    def __bs64_save_attachments(self,base64str:str,header_data:str,output_path:str="Resources/TEMP_FILES"):
-        meta_data:dict = {}
-        name = "temp_file"
-
-        #extracts Meta-Data
-        for field in str(header_data).split('; '):
-            if '=' not in field:
-                continue
-            field = field.split('"')
-            meta_data[field[0]] = field[1]
-            if "name" in field[0]:
-                name = field[1]
-        
-        #removes Mime Header
-        if "," in base64str:
-            base64str = base64str.split(",", 1)[1]
-        
-        #writes bytes to file
-        file_bytes = base64.b64decode(base64str)
-
-        if path.exists(output_path) == False:
-            mkdir(output_path)
-
-        with open(f"{output_path}/{name}", "wb") as f:
-            f.write(file_bytes)
-
-        return meta_data
-
-    def __repr__(self):
-        return f"Email<Subject:{self.subject},Sender:{self.sender}>"
-
-
-
-
+from LangAnalysis import Email
 
 #### IDEA:
 # be able to add/remove stuff into WORDLISTS
@@ -205,7 +75,7 @@ class UrlCheck(Email):
     # extract wordlist
     # REMEMBER TO CHANGE FILE PATH
     def extract_wordlist(self, filename=None):
-        with open(f'URLChecking/temp_wordlist/{filename}', "r", encoding="utf-8") as f:
+        with open(f'Resources/WORDLISTS/temp_wordlist{filename}', "r", encoding="utf-8") as f:
             wordlist = f.read().split()
 
         return wordlist
